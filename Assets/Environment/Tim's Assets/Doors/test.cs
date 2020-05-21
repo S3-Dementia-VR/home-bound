fb13df03ecf12acb0d46138ea8139d9b71236f78
pllmchll <pllmchllssn@gmail.com>
2020-05-21 13:39:39 +0800

    door slotted

---
+++ Assets/Environment/Tim's Assets/Doors/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 size = GetComponent<Collider>().bounds.size;
+        Debug.Log(size.x);
+        Debug.Log(size.y);
+        Debug.Log(size.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
