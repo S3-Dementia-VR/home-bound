ca42ec2e802e2e160fcbd755094c21ed436d3f33
pllmchll <pllmchllssn@gmail.com>
2020-06-16 07:48:15 +0800

    sitting interaction (terrible)

--- Assets/Scripts/interaction/sitControl.cs
+++ Assets/Scripts/interaction/sitControl.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class sitControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject fps;
+    public Transform chairT;
+    private MeshCollider chairC;
+    private MeshCollider floorC;
+    private FirstPersonAIO fpsScript;
+    private Transform fpsPos;                    // stores character position
+    private Vector3 tempPos = new Vector3 (0,0,0);
+    private bool sitting = false;
+
+
+    void Start() {
+        fps = GameObject.Find("FirstPerson-AIO");
+        fpsScript = fps.GetComponent<FirstPersonAIO>();
+        fpsPos = fps.GetComponent<Transform>();
+        chairT = GetComponent<Transform>();
+        chairC = GetComponent<MeshCollider>();
+        floorC = GameObject.Find("floor_main").GetComponent<MeshCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Vector3.Distance(chairT.position, fpsPos.position) <= 15) {
+            if (Input.GetButtonDown("Interact")) {
+                if (sitting == false) {
+                    tempPos = fpsPos.position;
+                    fpsScript.playerCanMove = false;
+                    chairC.enabled = false;
+                    sitting = true;
+                    fpsPos.position = chairT.position;
+                    floorC.enabled = false;
+                } 
+                else {
+                    fpsScript.playerCanMove = true;
+                    chairC.enabled = true;
+                    sitting = false;
+                    fpsPos.position = tempPos;
+                    floorC.enabled = true;
+                }
+            }
+        }
     }
 }
