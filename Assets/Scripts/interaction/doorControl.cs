91404f77cf19641f4238e35367e621637a72f7ce
pllmchll <pllmchllssn@gmail.com>
2020-06-15 17:19:32 +0800

    door works kinda

---
+++ Assets/Scripts/interaction/doorControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorControl : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        anim.speed = 2;
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Interact")) {
+            if(anim.GetBool("open") == false) {
+                anim.Play("Door_open");
+                anim.SetBool("open", true);
+            }
+            else {
+                anim.Play("Door_Close");
+                anim.SetBool("open", false);
+            }
+        }
+    }
+}
