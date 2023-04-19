ec640c8eb1f2ef7b2b976f9d48991146a565698c
GreatLore <glonnie0027@gmail.com>
2023-04-19 08:33:38 +0800

    QOL changes
    smoothed out controls.  Rearranged and added more collective memory objects.

--- Assets/loc_controller_script.cs
+++ Assets/loc_controller_script.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class loc_controller_script : MonoBehaviour
 {
     public GameObject small_living_room;
+    public GameObject small_dining_room;
+    public GameObject small_garden_room;
+    public GameObject use_grab_interactor_right1;
+    public GameObject use_grab_interactor_right2;
     public float time_out;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,13 @@ public class loc_controller_script : MonoBehaviour
     public void switch_LivingRoom(){
         StartCoroutine( switch_LivingRoom_coroutine() );
     }
+    public void switch_DiningRoom(){
+        StartCoroutine( switch_DiningRoom_coroutine() );
+    }
+    public void switch_GardenRoom(){
+        StartCoroutine( switch_GardenRoom_coroutine() );
+    }
+
     public IEnumerator switch_LivingRoom_coroutine(){
         yield return new WaitForSeconds(time_out);
         if (small_living_room.activeSelf){
@@ -27,6 +38,31 @@ public class loc_controller_script : MonoBehaviour
         }
         else{
             small_living_room.SetActive(true);
+        }
+        yield return null;
+    }
+    
+    public IEnumerator switch_DiningRoom_coroutine(){
+        yield return new WaitForSeconds(time_out);
+        if (small_dining_room.activeSelf){
+            small_dining_room.SetActive(false);
+        }
+        else{
+            small_dining_room.SetActive(true);
+        }
+        yield return null;
+    }
+    public IEnumerator switch_GardenRoom_coroutine(){
+        yield return new WaitForSeconds(time_out);
+        if (small_garden_room.activeSelf){
+            small_garden_room.SetActive(false);
+            use_grab_interactor_right1.SetActive(false);
+            use_grab_interactor_right2.SetActive(false);
+        }
+        else{
+            small_garden_room.SetActive(true);
+            use_grab_interactor_right1.SetActive(true);
+            use_grab_interactor_right2.SetActive(true);
         }
         yield return null;
     }
