ed87ea904085748e1d5ca3807790dba1b5a4157b
GreatLore <glonnie0027@gmail.com>
2023-04-20 16:51:53 +0800

    Initial commit of nature assets
    Incorporated nature assets. Added chair teleportation in garden area. Fixed graphic issues.

--- Assets/loc_controller_script.cs
+++ Assets/loc_controller_script.cs
@@ -9,6 +9,7 @@ public class loc_controller_script : MonoBehaviour
     public GameObject small_garden_room;
     public GameObject use_grab_interactor_right1;
     public GameObject use_grab_interactor_right2;
+    public GameObject nature_view;
     public float time_out;
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,9 @@ public class loc_controller_script : MonoBehaviour
     }
     public void switch_GardenRoom(){
         StartCoroutine( switch_GardenRoom_coroutine() );
+    }
+    public void switch_NatureView(){
+        StartCoroutine( switch_NatureView_coroutine() );
     }
 
     public IEnumerator switch_LivingRoom_coroutine(){
@@ -52,6 +56,7 @@ public class loc_controller_script : MonoBehaviour
         }
         yield return null;
     }
+
     public IEnumerator switch_GardenRoom_coroutine(){
         yield return new WaitForSeconds(time_out);
         if (small_garden_room.activeSelf){
@@ -63,6 +68,17 @@ public class loc_controller_script : MonoBehaviour
             small_garden_room.SetActive(true);
             use_grab_interactor_right1.SetActive(true);
             use_grab_interactor_right2.SetActive(true);
+        }
+        yield return null;
+    }
+    
+    public IEnumerator switch_NatureView_coroutine(){
+        yield return new WaitForSeconds(time_out);
+        if (nature_view.activeSelf){
+            nature_view.SetActive(false);
+        }
+        else{
+            nature_view.SetActive(true);
         }
         yield return null;
     }
